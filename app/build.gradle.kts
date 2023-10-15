7ec16a0c4654ad6403dc4b8e757e25cfc7bb96bd
CrowForKotlin <CrowForKotlin@gmail.com>
2023-10-15 18:46:12 +0800

    ◉ :construction: 增加内部版本配置

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -116,6 +116,19 @@ android {
         // 维度 抽象的概念，表示应用程序的不同版本
         flavorDimensions.add(AppConfigs.flavor_dimension)
 
+        // 内部版本
+        create(AppConfigs.flavor_internal) {
+            // 指定维度
+            dimension = AppConfigs.flavor_dimension
+
+            // 版本名后缀
+            versionNameSuffix = "_internal"
+
+            // 是否使用线上环境
+            buildConfigField("boolean", "IS_ONLINE_ENV", "true")
+
+        }
+
         // 正式线上版本
         create(AppConfigs.flavor_online) {
 
@@ -145,8 +158,6 @@ android {
             buildConfigField("boolean", "IS_ONLINE_ENV", "false")
         }
     }
-
-
 }
 
 dependencies {
--- buildSrc/src/main/kotlin/AppConfigs.kt
+++ buildSrc/src/main/kotlin/AppConfigs.kt
@@ -75,10 +75,10 @@ object AppConfigs {
     const val version_code = 10
 
     // 应用版本名
-    const val version_name = "1.2.6"
+    const val version_name = "1.2.7"
 
     // jvm版本
-    const val jvm_target = "11"
+    const val jvm_target = "12"
 
     // Android 提供的默认测试工具
     const val test_instrumentation_runner = "androidx.test.runner.AndroidJUnitRunner"
@@ -97,6 +97,9 @@ object AppConfigs {
 
     // 开发版本
     const val flavor_dev = "dev"
+
+    // 内部版本
+    const val flavor_internal = "internal"
 
     // 设置代码源名称
     const val source_set_main = "main"
