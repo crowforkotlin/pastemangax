5e16b65a3f881cfe407b55ed155b7e1d8dcd4b08
WuYa <crowforkotlin@gmail.com>
2023-12-14 01:54:11 +0800

    :fire: 优化简繁转换

--- lib_mangax/src/main/kotlin/com/crow/mangax/tools/language/ChineseConverter.kt
+++ lib_mangax/src/main/kotlin/com/crow/mangax/tools/language/ChineseConverter.kt
@@ -1,119 +1,79 @@
+@file:Suppress("SameParameterValue")
+
 package com.crow.mangax.tools.language
 
 import android.content.Context
-import android.util.Log
 import com.crow.base.app.app
+import com.crow.base.tools.extensions.copyFolder
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
-import java.io.InputStream
-import java.io.OutputStream
 import java.util.concurrent.Executors
 
 /**
- * Created by zhangqichuan on 29/2/16.
+ * ● 中文转化工具
+ *
+ * ● 2023-12-14 01:51:12 周四 上午
+ * @author crowforkotlin
  */
 object ChineseConverter {
 
+    /**
+     * ● 转换协程作用域
+     *
+     * ● 2023-12-14 01:44:12 周四 上午
+     * @author crowforkotlin
+     */
+    private val mConvertScope = CoroutineScope(Executors.newSingleThreadExecutor().asCoroutineDispatcher())
+
+    /**
+     * ● 加载动态库
+     *
+     * ● 2023-12-14 01:46:59 周四 上午
+     * @author crowforkotlin
+     */
     init { System.loadLibrary("ChineseConverter") }
 
-    fun init() {
-        val lastDataFile = File(app.filesDir.toString() + "/opencc_data/zFinished2")
-        if (!lastDataFile.exists()) {
-            initialize(app)
+    /**
+     * ● 初始化词典文件夹
+     *
+     * ● 2023-12-14 01:49:17 周四 上午
+     * @author crowforkotlin
+     */
+    private fun initialize(context: Context) {
+        mConvertScope.launch(Dispatchers.IO) {
+            val lastDataFile = File(app.filesDir.toString() + "/opencc_data/zFinished2")
+            if (!lastDataFile.exists()) {
+                initialize(app)
+            }
+            context.copyFolder("opencc_data")
         }
     }
 
-    private val mConvertScope = CoroutineScope(Executors.newSingleThreadExecutor().asCoroutineDispatcher())
-    /***
-     * @param text           the text to be converted to
-     * @param conversionType the conversion type
-     * @param context        android context
-     * @return the converted text
+    /**
+     * ● 清除词典数据文件夹，仅在更新词典数据时调用该方法。
+     *
+     * ● 2023-12-14 01:48:27 周四 上午
+     * @author crowforkotlin
+     */
+    fun clearDictDataFolder(context: Context) {
+        File(context.filesDir.toString(), "opencc_data").deleteRecursively()
+    }
+
+    /**
+     * ● 转换文本
+     *
+     * ● 2023-12-14 01:47:43 周四 上午
+     * @author crowforkotlin
      */
     suspend fun convert(text: String, conversionType: ConversionType = ConversionType.HK2S): String {
         return mConvertScope.async { nativeConvert(text, "${app.filesDir.absolutePath}/opencc_data/${conversionType.value}") }.await()
     }
 
-    /***
-     * Clear the dictionary data folder, only call this method when update the dictionary data.
-     * @param context
-     */
-    fun clearDictDataFolder(context: Context) {
-        val dataFolder = File(context.filesDir.toString() + "/opencc_data")
-        deleteRecursive(dataFolder)
-    }
-
-    private fun deleteRecursive(fileOrDirectory: File) {
-        if (fileOrDirectory.isDirectory()) for (child in fileOrDirectory.listFiles()) deleteRecursive(
-            child
-        )
-        fileOrDirectory.delete()
-    }
-
     private external fun convert(text: String, configFile: String, absoluteDataFolderPath: String): String
 
     private external fun nativeConvert(text: String, filePath: String): String
-
-    private fun initialize(context: Context) {
-        copyFolder("opencc_data", context)
-    }
-
-    private fun copyFolder(folderName: String, context: Context) {
-        val fileFolderOnDisk = File(context.filesDir.toString() + "/" + folderName)
-        val assetManager = context.assets
-        var files: Array<String>? = null
-        try {
-            files = assetManager.list(folderName)
-        } catch (e: IOException) {
-            Log.e("tag", "Failed to get asset file list.", e)
-        }
-        if (files != null) {
-            for (filename in files) {
-                var `in`: InputStream? = null
-                var out: OutputStream? = null
-                try {
-                    `in` = assetManager.open("$folderName/$filename")
-                    if (!fileFolderOnDisk.exists()) {
-                        fileFolderOnDisk.mkdirs()
-                    }
-                    val outFile = File(fileFolderOnDisk.absolutePath, filename)
-                    if (!outFile.exists()) {
-                        outFile.createNewFile()
-                    }
-                    out = FileOutputStream(outFile)
-                    copyFile(`in`, out)
-                } catch (e: IOException) {
-                    Log.e("tag", "Failed to copy asset file: $filename", e)
-                } finally {
-                    if (`in` != null) {
-                        try {
-                            `in`.close()
-                        } catch (e: IOException) {
-                            // NOOP
-                        }
-                    }
-                    if (out != null) {
-                        try {
-                            out.close()
-                        } catch (e: IOException) {
-                            // NOOP
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    @Throws(IOException::class)
-    private fun copyFile(`in`: InputStream, out: OutputStream) {
-        val buffer = ByteArray(1024)
-        var read: Int
-        while (`in`.read(buffer).also { read = it } != -1) {
-            out.write(buffer, 0, read)
-        }
-    }
 }
