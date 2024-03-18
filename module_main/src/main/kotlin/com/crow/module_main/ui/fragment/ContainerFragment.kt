7677105d1919e7c3d51af2263b0e3751c8d129a5
WuYa <crowforkotlin@gmail.com>
2024-03-18 21:29:36 +0800

    :fire: 修复垂直翻页遗留问题

--- module_main/src/main/kotlin/com/crow/module_main/ui/fragment/ContainerFragment.kt
+++ module_main/src/main/kotlin/com/crow/module_main/ui/fragment/ContainerFragment.kt
@@ -1,10 +1,6 @@
 package com.crow.module_main.ui.fragment
 
-import android.R.attr.bitmap
 import android.annotation.SuppressLint
-import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Base64
 import android.view.GestureDetector
@@ -20,7 +16,6 @@ import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.withResumed
 import androidx.viewpager2.widget.ViewPager2
 import com.crow.base.R.string.base_exit_app
 import com.crow.base.tools.coroutine.FlowBus
@@ -35,7 +30,6 @@ import com.crow.base.tools.extensions.doOnClickInterval
 import com.crow.base.tools.extensions.doOnInterval
 import com.crow.base.tools.extensions.immersionPadding
 import com.crow.base.tools.extensions.isLatestVersion
-import com.crow.base.tools.extensions.log
 import com.crow.base.tools.extensions.navigateToWithBackStack
 import com.crow.base.tools.extensions.newMaterialDialog
 import com.crow.base.tools.extensions.onCollect
